14e5dab1a84658fad2b525a6f173a71018510c6a
王天禹 <15663583946@163.com>
2017-10-09 19:38:53 +0800

    app的record页面

--- js/index.js
+++ js/index.js
@@ -1,8 +1,6 @@
 /**
  * Created by 王天禹 on 2017/10/9.
  */
-(function(){
-    var oDiv = document.getElementById("footer");
-    var aLi = document.getElementsByTagName("li");
+$(function(){
 
-})();
+});
